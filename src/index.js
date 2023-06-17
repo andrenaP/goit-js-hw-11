3d5333ff559831894fa6ee698a6d5493b826539d
andrenaP <a0993100106@gmail.com>
2023-06-17 22:26:56 +0300

    That the end

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 import axios from 'axios';
 import SimpleLightbox from "simplelightbox";
 import Notiflix from 'notiflix';
-
+import "simplelightbox/dist/simple-lightbox.min.css";
 import ApiServiceClass from './js/get_img'
-
 var throttle = require('lodash.throttle');
 const ApiService= new ApiServiceClass();
 
 const form = document.forms[0];
 const submit_button = form.querySelector('button[type="submit"]');
 const input_text= form.querySelector('input[type="text"]');
-const load_more=document.querySelector(".load-more");
+// const load_more=document.querySelector(".load-more");
 const gallery=document.querySelector(".gallery");
+let simpleLightBox;
 
 submit_button.addEventListener('click', OnSubmit)
-load_more.addEventListener('click', OnLoad)
+// load_more.addEventListener('click', OnLoad)
+
+window.onscroll = function(ev) {
+  if ((window.innerHeight + Math.round(window.scrollY)) >= document.body.offsetHeight) {
+    do_it()
+  }
+};
 
 const lightbox = new SimpleLightbox('.gallery .photo-card', {
   caption: true,
@@ -23,32 +29,51 @@ const lightbox = new SimpleLightbox('.gallery .photo-card', {
 });
 
 
-disbtn(load_more,false)
+// disbtn(load_more,false)
 input_text.addEventListener("input",throttle((event) => {disbtn(submit_button,false)}),100);
 
 function OnLoad(event){
   event.preventDefault();
-  do_it(input_text.value);
+  do_it();
 }
 
 function OnSubmit(event){
   event.preventDefault();
+  if (input_text.value.trim()==="")
+  {Notiflix.Notify.failure('The search string cannot be empty. Please specify your search query.');
+return;}
+  
   clearAll();
   ApiService.ResetPage();
-  do_it(input_text.value);
+  do_it();
   disbtn(submit_button,true)
-  disbtn(load_more,false)
+  // disbtn(load_more,false)
+
 
 }
 
-async function do_it(call_n)
-{
-  
-  ApiService.query=call_n;
-  const info=await ApiService.fetchArticles()
-  // console.log(info);
-  createHTML(info.data.hits);
 
+async function do_it()
+{
+  if (simpleLightBox)
+  simpleLightBox.destroy();
+  //simpleLightBox.refresh();
+  try{
+  ApiService.query=input_text.value;
+  const info=await ApiService.fetchArticles()
+ 
+    
+  
+   console.log(info);
+   if (gallery.innerHTML ==="")
+   Notiflix.Notify.success(`Hooray! We found ${info.data.totalHits} images.`);
+  createHTML(info.data.hits);
+}
+catch(e){
+  Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.")
+  simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+  window.onscroll=null;
+}
 }
 function createHTML(data){
   console.log(Object.assign(data));
@@ -57,12 +82,19 @@ function createHTML(data){
   )
   .join("");
   gallery.insertAdjacentHTML('beforeend', readyHtml);
-  lightbox.refresh();
+  simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+  const { height: cardHeight } = document
+  .querySelector(".gallery")
+  .firstElementChild.getBoundingClientRect();
+  window.scrollBy({
+    top: cardHeight * 1,
+    behavior: "smooth",
+  });
 }
 function To_Html(c){
 
  return `<a class="photo-card" href=${c.largeImageURL}>
-  <img src="${c.largeImageURL}" alt="${c.tags}" loading="lazy" />
+  <img src="${c.webformatURL}" alt="${c.tags}" loading="lazy" />
   <div class="info">
     <p class="info-item">
       <b>Likes ${c.likes}</b>
--- src/js/get_img.js
+++ src/js/get_img.js
@@ -7,7 +7,7 @@ export default class GetApiService{
     async fetchArticles(){
         const URL ="https://pixabay.com/api/";
         const KEY ="37324716-561554bcb566b8f1f5945e5c9";
-        const result=await  axios.get(`${URL}?key=${KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&`);
+        const result=await  axios.get(`${URL}?key=${KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`);
         this.page+=1;
         console.log(this.page);
         const info=  await result
